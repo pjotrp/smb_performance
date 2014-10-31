0e21fa2e6f323c0ac81ac967a8ea63cb1fe22be6
Pjotr Prins <pjotr.prins@thebird.nl>
2014-10-31 15:29:40 +0100

    Scripts

---
+++ scripts/sam_index.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# ./scripts/sam_index.sh &>> results/sam_index.out
+
+echo "Indexing with samtools"
+date
+
+. etc/environment
+
+for y in 1 2 3 ; do
+  cmd="$HOME/opt/bin/samtools index $bam16s"
+  echo $cmd
+  time $($cmd)
+done
+
+date
--- scripts/smb_index.sh
+++ scripts/smb_index.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+echo "Indexing with samtools"
+. etc/environment
+
 for y in 1 2 3 ; do
-  echo samtools index data/1000genomes/HG00110.mapped.ILLUMINA.bwa.GBR.low_coverage.20130415.bam
-  time ~/opt/bin/samtools index data/1000genomes/HG00110.mapped.ILLUMINA.bwa.GBR.low_coverage.20130415.bam
-  for x in 47 31 15 7 3 1 0 ; do
-    cmd="$HOME/opt/bin/sambamba index -t $x data/1000genomes/HG00110.mapped.ILLUMINA.bwa.GBR.low_coverage.20130415.bam"
-    echo $y $x $cmd
-    time `$cmd`
-  done
+  cmd="$HOME/opt/bin/samtools index $bam16"
+  time $($cmd)
 done
 
