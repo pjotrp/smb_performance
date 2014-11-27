39cacaa39f739c157ba7d019e47bedabc90103f2
Pjotr Prins <pjotr.public01@thebird.nl>
2014-11-27 19:12:39 +0100

    Unsorted rmdup

--- scripts/smb_markdup.sh
+++ scripts/smb_markdup.sh
@@ -8,7 +8,7 @@ date
 . etc/environment
 
 for x in 47 31 15 7 ; do
-  cmd="$sambamba markdup -t $x -r --overflow-list-size 500000 $bamsort31s smb_rmdup.bam"
+  cmd="$sambamba markdup -t $x -r --overflow-list-size 500000 $bam31s smb_rmdup.bam"
   echo $cmd
   $(/usr/bin/time -v $cmd)
 done
