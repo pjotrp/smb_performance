3a99aea6b9f16d39f2e77e7e6b5bf4cd386a5ad0
Pjotr Prins <pjotr.public01@thebird.nl>
2014-11-30 10:41:42 +0100

    mpileup tests

---
+++ scripts/sam_mpileup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# ./scripts/sam_mpileup.sh &>> results/sam_mpileup.out
+
+echo "mpileuping with samtools"
+date
+
+. etc/environment
+
+for y in 1 2 3 ; do
+  cmd="$samtools mpileup --VCF -o sam_mpileup.vcf  -gu -S -D -d 1000 -L 1000 -m 3 -F 0.0002 $mpileup_files"
+  echo $cmd
+  $(/usr/bin/time -v $cmd)
+done
+
+date
--- scripts/smb_mpileup.sh
+++ scripts/smb_mpileup.sh
@@ -8,8 +8,8 @@ date
 . etc/environment
 
 for x in 47 31 15 7 ; do
-  # cmd="$sambamba mpileup -t $x -o smb_mpileup.mpileup $mpileup_files"
-  cmd="$sambamba mpileup -t $x -o smb_mpileup.mpileup $bam31s --bcftools"
+  cmd="$sambamba mpileup -t $x -o smb_mpileup.mpileup $mpileup_files --bcftools"
+  # cmd="$sambamba mpileup -t $x -o smb_mpileup.mpileup $bam31s --bcftools"
   echo $cmd
   $(/usr/bin/time -v $cmd)
 done
