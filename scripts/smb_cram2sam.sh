76032256aa2fb65a0945f3d331b5c628966a3ce8
Pjotr Prins <pjotr.public01@thebird.nl>
2014-12-10 19:33:15 +0100

    tmpfs

--- scripts/smb_cram2sam.sh
+++ scripts/smb_cram2sam.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
 # ./scripts/smb_cram2sam.sh &>> results/smb_cram2sam.out
+#
+# To run on tmpfs create
+#
+#   mount -t tmpfs -o size=128g tmpfs /mnt/ramdisk
 
 echo "cram2sam with sambamba"
 date
@@ -8,7 +12,8 @@ date
 . etc/environment
 
 for t in 47 31 15 7 3 0; do
-  outfn=smb_cram2sam_$t.sam
+  # outfn=/mnt/ramdisk/smb_cram2sam_$t.sam
+  outfn=/mnt/ramdisk/smb_cram2sam.sam
   cmd="$sambamba view -t $t -C $cram10s -o $outfn"
   echo $cmd
   $(/usr/bin/time -v $cmd)
